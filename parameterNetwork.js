9f537219585296a7ccd19a2ed23090a540c9e064
stjesnay <jennifer.snay@stud.h-da.de>
2021-06-27 13:57:22 +0200

    branch test

--- parameterNetwork.js
+++ parameterNetwork.js
@@ -17,7 +17,9 @@ export default class ParameterNetwork {
     this.beifang = 80;
     this.co2 = 80;
   }
-  inputToOutput() {} //welche input parameter wirken sich auf welche output parameter aus, was sink und was steigt wenn was geändert wird
+  inputToOutput() {
+    console.log("jenny working");
+  } //welche input parameter wirken sich auf welche output parameter aus, was sink und was steigt wenn was geändert wird
   calculateScore() {} //was für ein score von 0% nachhaltigkeit bis 100% nachhaltigkeit wird durch summe aus output parameter erreicht; return score an visualize
   getOutput() {} //methode noch nicht safe. Idee: Alle outputs in einem Array zusammensetzen um ein Array abfragen zu können (in visualize) und nicht jeden output einzeln
 }
