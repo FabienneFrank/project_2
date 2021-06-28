9a73ac9928b57ff2add18f0cfeb0ce0ae4f1aeed
stjesnay <jennifer.snay@stud.h-da.de>
2021-06-28 17:32:16 +0200

    start und endscreen aufbau(endscreen nicht fertig)

--- button.js
+++ button.js
@@ -2,11 +2,10 @@
 let myFontDefault = loadFont("font/D-DINCondensed.otf");
 let myFontBold = loadFont("font/D-DINCondensed-Bold.otf");
 export default class Button {
-  constructor(x, y, color, hoverColor, lable) {
+  constructor(x, y, color, lable) {
     this.x = x;
     this.y = y;
     this.color = color;
-    this.hoverColor = hoverColor;
     this.lable = lable;
   }
   default() {
@@ -22,6 +21,33 @@ export default class Button {
     text(this.lable, this.x, this.y + 60);
   } // start, replay, home, back to game
   exit() {} //exit pfeil
-  scroll() {} //endscreen runde buttons zu Tips scrollen
-  hitTest() {}
+  scroll() {
+    noFill();
+    strokeWeight(5);
+    stroke(this.color);
+    ellipse(this.x, this.y + 180, 50, 50);
+    line(this.x - 10, this.y + 175, this.x, this.y + 185);
+    line(this.x + 10, this.y + 175, this.x, this.y + 185);
+    rect(this.x, this.y + 300, 600, 600);
+  } //endscreen runde buttons zu Tips scrollen
+  hitTest() {
+    if (this.lable === "Start" || this.lable === "Replay") {
+      if (
+        mouseX > this.x - 150 &&
+        mouseX < this.x + 150 &&
+        mouseY > this.y &&
+        mouseY < this.y + 80
+      ) {
+        fill(83, 124, 123);
+        this.color = color(247, 240, 226);
+        rect(this.x - 150, this.y, 300, 80, 20);
+        return true;
+      } else {
+        noFill();
+        this.color = color(251, 84, 82);
+        rect(this.x - 150, this.y, 300, 80, 20);
+        return false;
+      }
+    }
+  }
 }
