42d23973dd46738cfb7f381b2ca0afb8f7ff9035
stjesnay <jennifer.snay@stud.h-da.de>
2021-06-29 13:37:10 +0200

    endscreen fertig (aussehen und hover tipps. keine Button click funktionen)

--- screens.js
+++ screens.js
@@ -13,25 +13,41 @@ export default class Screens {
       "Super gemacht! Das Ziel ist erreicht, aber kannst du das Ziel auch übertreffen?",
       "WOW das lief ja fantastisch!",
     ];
+    this.tippstext = [];
+
     //--------------------------
     this.visualize = new Visualize(this.x, this.y);
     this.startButton = new Button(
       this.width / 2,
       (this.height * 2) / 3,
       color(251, 84, 82),
-      "Start"
+      "Start",
+      color(30, 130, 160, 255),
+      "default"
     );
     this.replayButton = new Button(
       this.width / 2,
       this.height / 2 + 75,
       color(251, 84, 82),
-      "Replay"
+      "Replay",
+      color(30, 140, 160, 255),
+      "default"
     );
     this.scrollDownButton = new Button(
       this.width / 2,
       this.height / 2 + 200,
       color(247, 240, 226),
-      "ScrollDown"
+      "ScrollDown",
+      color(30, 150, 160, 255),
+      "round"
+    );
+    this.scrollUpButton = new Button(
+      this.width / 2,
+      this.height / 2 + 1200,
+      color(247, 240, 226),
+      "ScrollUp",
+      color(30, 160, 160, 255),
+      "round"
     );
   }
   startScreen() {
@@ -51,8 +67,13 @@ export default class Screens {
       this.height / 2 - 100
     );
     text(
-      "Steig in die Rolle eines global politischen Akteurs und finde heraus wie die Welt durch",
-      this.width / 2,
+      "Steig in die Rolle eines",
+      this.width / 2 - 430,
+      this.height / 2 - 50
+    );
+    text(
+      "und finde heraus wie die Welt durch",
+      this.width / 2 + 341,
       this.height / 2 - 50
     );
     text(
@@ -61,14 +82,19 @@ export default class Screens {
       this.height / 2
     );
     text(
-      "Agenda 2030: Das Leben Unterwasser erhalten",
-      this.width / 2,
+      "Das Leben Unterwasser erhalten",
+      this.width / 2 + 93,
       this.height / 2 + 50
     );
+    fill(251, 84, 82);
+    text(
+      "global politischen Akteurs",
+      this.width / 2 - 90,
+      this.height / 2 - 50
+    );
+    text("Agenda 2030: ", this.width / 2 - 222, this.height / 2 + 50);
     //Start-Button
-    if (this.startButton.hitTest()) {
-      //change Screen
-    }
+    this.startButton.interactionarea();
     this.startButton.default();
   } // Jenny
   gameScreen() {
@@ -88,9 +114,7 @@ export default class Screens {
     textAlign(CENTER);
     text("Score: " + "x", this.width / 2, this.height / 2 - 390);
     //Replay-Button
-    if (this.replayButton.hitTest()) {
-      //change Screen
-    }
+    this.replayButton.interactionarea();
     this.replayButton.default();
     //Motivationstext
     fill(247, 240, 226);
@@ -107,9 +131,148 @@ export default class Screens {
       text(this.motivationstext[2], this.width / 2, this.height / 2 + 250);
     }
     //Scroll-Down-Button
-    this.scrollDownButton.hitTest();
+    this.scrollDownButton.interactionarea();
     this.scrollDownButton.scroll();
+    //Tipps
+    fill(247, 240, 226);
+    rect(this.width / 2 - 525, this.height / 2 + 600, 250, 250, 20);
+    rect(this.width / 2 - 125, this.height / 2 + 600, 250, 250, 20);
+    rect(this.width / 2 + 275, this.height / 2 + 600, 250, 250, 20);
+    rect(this.width / 2 - 525, this.height / 2 + 1000, 250, 250, 20);
+    rect(this.width / 2 - 125, this.height / 2 + 1000, 250, 250, 20);
+    rect(this.width / 2 + 275, this.height / 2 + 1000, 250, 250, 20);
+    this.displayTipps();
+    //Scroll-Up-Button
+    this.scrollUpButton.interactionarea();
+    this.scrollUpButton.scroll();
   } // Jenny: Get Score
   exitPopUp() {}
   tutorial() {} //Fabi
+  displayTipps() {
+    textAlign(CENTER);
+    noStroke();
+    textFont(myFontBold);
+    //Tipp1
+    if (
+      mouseX > this.width / 2 - 525 &&
+      mouseX < this.width / 2 - 275 &&
+      mouseY > this.height / 2 + 600 &&
+      mouseY < this.height / 2 + 850
+    ) {
+      fill(5, 85, 83);
+      textSize(30);
+      text("Versuche bei deinem", this.width / 2 - 400, this.height / 2 + 650);
+      text("Einkauf auf", this.width / 2 - 400, this.height / 2 + 690);
+      text("Fangmethoden und", this.width / 2 - 400, this.height / 2 + 730);
+      text("-gebiete zu achten", this.width / 2 - 400, this.height / 2 + 770);
+    } else {
+      fill(244, 151, 151);
+      textSize(70);
+      text("Tipp #1", this.width / 2 - 400, this.height / 2 + 750);
+    }
+    //Tipp2
+    if (
+      mouseX > this.width / 2 - 125 &&
+      mouseX < this.width / 2 + 125 &&
+      mouseY > this.height / 2 + 600 &&
+      mouseY < this.height / 2 + 850
+    ) {
+      fill(5, 85, 83);
+      textSize(30);
+      text("Unterstütze doch", this.width / 2, this.height / 2 + 650);
+      text("lieber deinen lokalen", this.width / 2, this.height / 2 + 690);
+      text("Fischhändler anstatt", this.width / 2, this.height / 2 + 730);
+      text("im Supermarkt deinen", this.width / 2, this.height / 2 + 770);
+      text("Fisch zu kaufen", this.width / 2, this.height / 2 + 810);
+    } else {
+      fill(244, 151, 151);
+      textSize(70);
+      text("Tipp #2", this.width / 2, this.height / 2 + 750);
+    }
+    //Tipp3
+    if (
+      mouseX > this.width / 2 + 275 &&
+      mouseX < this.width / 2 + 525 &&
+      mouseY > this.height / 2 + 600 &&
+      mouseY < this.height / 2 + 850
+    ) {
+      fill(5, 85, 83);
+      textSize(30);
+      text("Alternative zu in", this.width / 2 + 400, this.height / 2 + 650);
+      text("Fisch enthaltener", this.width / 2 + 400, this.height / 2 + 690);
+      text("Omega-3-Fettsäure:", this.width / 2 + 400, this.height / 2 + 730);
+      text(" Leinsamen,Rapsöl,", this.width / 2 + 400, this.height / 2 + 770);
+      text("Walnüssen o. Algen", this.width / 2 + 400, this.height / 2 + 810);
+    } else {
+      fill(244, 151, 151);
+      textSize(70);
+      text("Tipp #3", this.width / 2 + 400, this.height / 2 + 750);
+    }
+    //Tipp4
+    if (
+      mouseX > this.width / 2 - 525 &&
+      mouseX < this.width / 2 - 275 &&
+      mouseY > this.height / 2 + 1000 &&
+      mouseY < this.height / 2 + 1250
+    ) {
+      fill(5, 85, 83);
+      textSize(30);
+      text("Aquakulturen sind", this.width / 2 - 400, this.height / 2 + 1050);
+      text(
+        "leider keine bessere",
+        this.width / 2 - 400,
+        this.height / 2 + 1090
+      );
+      text("Alternative zu", this.width / 2 - 400, this.height / 2 + 1130);
+      text(
+        "industrieller Fischerei",
+        this.width / 2 - 400,
+        this.height / 2 + 1170
+      );
+    } else {
+      fill(244, 151, 151);
+      textSize(70);
+      text("Tipp #4", this.width / 2 - 400, this.height / 2 + 1150);
+    }
+    //Tipp5
+    if (
+      mouseX > this.width / 2 - 125 &&
+      mouseX < this.width / 2 + 125 &&
+      mouseY > this.height / 2 + 1000 &&
+      mouseY < this.height / 2 + 1250
+    ) {
+      fill(5, 85, 83);
+      textSize(30);
+      text("Informiere dich beim", this.width / 2, this.height / 2 + 1050);
+      text("Kauf eines Fisches", this.width / 2, this.height / 2 + 1090);
+      text("über seine Bestände", this.width / 2, this.height / 2 + 1130);
+    } else {
+      fill(244, 151, 151);
+      textSize(70);
+      text("Tipp #5", this.width / 2, this.height / 2 + 1150);
+    }
+    //Tipp6
+    if (
+      mouseX > this.width / 2 + 275 &&
+      mouseX < this.width / 2 + 525 &&
+      mouseY > this.height / 2 + 1000 &&
+      mouseY < this.height / 2 + 1250
+    ) {
+      fill(5, 85, 83);
+      textSize(30);
+      text("Betrachte doch", this.width / 2 + 400, this.height / 2 + 1050);
+      text(
+        "deinen Fisch als eine",
+        this.width / 2 + 400,
+        this.height / 2 + 1090
+      );
+      text("besondere Mahlzeit", this.width / 2 + 400, this.height / 2 + 1130);
+      text("und keine tägliche", this.width / 2 + 400, this.height / 2 + 1170);
+      text("Speise", this.width / 2 + 400, this.height / 2 + 1210);
+    } else {
+      fill(244, 151, 151);
+      textSize(70);
+      text("Tipp #6", this.width / 2 + 400, this.height / 2 + 1150);
+    }
+  }
 }
--- visualize.js
+++ visualize.js
@@ -11,7 +11,7 @@ export default class Visualize {
     this.interactionDict = new Map();
     //--------------------------
     this.mousePositionColor = [];
-    this.interactionKey = '';
+    this.interactionKey = "";
 
     let fischArray = [];
     let koralleArray = [];
@@ -20,32 +20,35 @@ export default class Visualize {
     let beifangArray = [];
   }
 
-  displayInteractionArea(){} //zeigt alle interactionAreas an
+  displayInteractionArea() {} //zeigt alle interactionAreas an
 
   //checkt Farben an der mausposition und pusht diese als Array [r,g,b,a]
-  colorCheck(){
-    this.mousePositionColor = get(mouseX,mouseY);
-  } 
+  colorCheck() {
+    this.mousePositionColor = get(mouseX, mouseY);
+  }
 
   //muss auserhalb der draw aufgerufen werden, setzt Keys und dazugehörige Farbcodes
-  interactionDictonary(){
-    this.interactionDict.set('boat', [100,90,110,255]);
-    this.interactionDict.set('haus', [100,80,120,255]);
+  interactionDictonary() {
+    this.interactionDict.set("boat", [100, 90, 110, 255]);
+    this.interactionDict.set("haus", [100, 80, 120, 255]);
+    this.interactionDict.set("startbutton", [30, 130, 160, 255]);
+    this.interactionDict.set("replaybutton", [30, 140, 160, 255]);
+    this.interactionDict.set("scrolldownbutton", [30, 150, 160, 255]);
+    this.interactionDict.set("scrollupbutton", [30, 160, 160, 255]);
   }
 
   //vergleicht zwei Arrays
-  compareArrays(array1,array2){
-    for (let i = 0; i <= array1.length; i++){
-      if(array1[i] !== array2[i])
-      return false;
+  compareArrays(array1, array2) {
+    for (let i = 0; i <= array1.length; i++) {
+      if (array1[i] !== array2[i]) return false;
     }
     return true;
   }
 
   //Checkt ob ein farbcode gerade angesprochen wird, wenn ja: interactionKey gesetzt
   checkKey() {
-    this.interactionDict.forEach((value,key) => {
-      if(this.compareArrays(this.mousePositionColor, value) === true){
+    this.interactionDict.forEach((value, key) => {
+      if (this.compareArrays(this.mousePositionColor, value) === true) {
         this.interactionKey = key;
       }
     });
@@ -53,7 +56,7 @@ export default class Visualize {
 
   doForKey(/*bekommt interactionKey*/) {} //bei welchem Farbcode soll welche Methode in keyAction ausgeführt werden
 
-  calculateEntities() {} //output prozente aus parameterNetwork abfragen und in anzahl an entities umwandeln, (bsp: 80% plastik = 1 Fisch, 60% plastik = 3 fische), anzahl entities in array speichern (3 Fische -> array[fisch1=new Fisch,fisch2=new Fisch,fisch3=new Fisch])
-  
-  diaplayVisuals(){} //zeigt die Oberfläche der Simulation an
+  calculateEntities() {} //Jenny: output prozente aus parameterNetwork abfragen und in anzahl an entities umwandeln, (bsp: 80% plastik = 1 Fisch, 60% plastik = 3 fische), anzahl entities in array speichern (3 Fische -> array[fisch1=new Fisch,fisch2=new Fisch,fisch3=new Fisch])
+
+  diaplayVisuals() {} //zeigt die Oberfläche der Simulation an
 }
